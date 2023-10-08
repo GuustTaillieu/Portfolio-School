ff2bf4a4cff72b14d2723ca57bdd1292844b5663
Guust Taillieu <guust@taillieusauw.net>
2023-10-08 11:50:35 +0200

    Made the hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 import { NextPage } from "next";
 import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="z-0 h-screen snap-y snap-mandatory overflow-y-scroll bg-background font-poppins text-white">
       <Head>
         <title>Portfolio Zita</title>
       </Head>
@@ -13,8 +14,14 @@ const Home: NextPage = () => {
         <Header />
 
         {/* HERO */}
+        <section id="hero" className="snap-start">
+          <Hero />
+        </section>
 
         {/* ABOUT */}
+        <section id="about" className="h-screen snap-center">
+          <h1>About</h1>
+        </section>
 
         {/* EXPERIENCE */}
 
