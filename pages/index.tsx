281f3fb9d9a944af5c36ca27dc8e7a0ce4309b7a
Guust Taillieu <guust@taillieusauw.net>
2023-10-12 23:47:25 +0200

    added scroll to top button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import About from "@/components/About";
 import ContactMe from "@/components/ContactMe";
 import Header from "@/components/Header";
@@ -5,19 +6,23 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
+import { ArrowUpCircleIcon } from "@heroicons/react/20/solid";
 import { NextPage } from "next";
 import Head from "next/head";
+import ScrollToTopButton from "@/components/ScrollToTopButton";
 
 const Home: NextPage = () => {
+  const hero = useRef<HTMLDivElement>(null);
+
   return (
-    <div className="scrollbar-track-gray-400/30 scrollbar-thumb-primary/80 scrollbar-thin z-0 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll bg-background font-poppins text-white">
+    <div className="z-0 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll bg-background font-poppins text-white scrollbar-thin scrollbar-track-gray-400/30 scrollbar-thumb-primary/80">
       <Head>
         <title>Portfolio Zita</title>
       </Head>
       <main>
         <Header />
 
-        <section id="hero" className="snap-start">
+        <section ref={hero} id="hero" className="snap-start">
           <Hero />
         </section>
 
@@ -40,6 +45,8 @@ const Home: NextPage = () => {
         <div id="contact" className="snap-start">
           <ContactMe />
         </div>
+
+        <ScrollToTopButton topElem={hero} />
       </main>
     </div>
   );
