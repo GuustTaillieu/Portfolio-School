5591e20c6c201682b5d2434b075d9afcbbd5ac18
Guust Taillieu <guust@taillieusauw.net>
2023-10-13 14:25:36 +0200

    Added sanity and the schemas

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import WorkExperience from "@/components/WorkExperience";
-import { ArrowUpCircleIcon } from "@heroicons/react/20/solid";
 import { NextPage } from "next";
 import Head from "next/head";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
